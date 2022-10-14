3ce099563bc8cda8b62068214f88dfef2a1cec87
FernandoCarreraCervantes <108480436+FernandoCarreraCervantes@users.noreply.github.com>
2022-10-13 23:52:56 -0500

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-12 col-md-3">
+            <h1>app vue</h1>
+
+        </div>
+        <div class="col-12 col-md-6">
+            <img :src="url" :alt="alt" width="100">
+        </div>
+    </div>
+    <hr>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            url: "",
+            alt: "Vue.js",
+        }),
+    };
+</script>
---
+++ src/components/TotalProyectos.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row">
+        <div class="col-6 my-2"> 
+            <h3>total proyectos: {{ numeroProyectos }}</h3>
+        </div>
+        <div class="col-6 text-end">
+            <button @click="limpiarData" class="btn btn-warning">limpiar proyectos</button>
+        </div>
+    </div>
+        <div class="table-responsive">
+            <table class="table table-dark" >
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>proyectos</th>
+                        <th>tipo</th>
+                        <th>urgente</th>
+                        <th>completado</th>
+                        <th>borrar</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr v-for="(proyecto, index) in proyectos" :key="index">
+                        <td>{{ index + 1 }}</td>
+                        <td>{{ proyecto.proyecto }}</td>
+                        <td>{{ proyecto.tipo }}</td>
+                        <td @click="cambiarEstado(proyecto, 'urgente')"  :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{ proyecto.urgente ? "Si"  :  "NO" }}</td>
+                        <td @click="cambiarEstado(proyecto, 'completado')"  :class="proyecto.completado ? 'bg-success' : 'bg-danger'">{{ proyecto.completado ? "Completo "  :  "Incompleto" }}</td>
+                        <td @click="borrarproyectos('borrar')" class="bg-warning" > borrar </td>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div> 
+</template>
+
+<script>
+    export default {
+        props: ['numeroProyectos', 'proyectos', 'cambiarEstado', 'limpiarData', 'borrarproyectos'],
+    };
+</script>
